4cd7c466e3178ec282ac0478a21f2ff6cc960b70
PotS <potsmugen@users.noreply.github.com>
2025-03-21 18:43:52 +0000

    fix: lost camera
    - Prevent the camera from tracking characters that have infinite position

--- src/char.go
+++ src/char.go
@@ -8373,10 +8373,12 @@ func (c *Char) track() {
 				}
 			}
 		}
-		if c.csf(CSF_movecamera_y) && !c.scf(SCF_standby) {
+		if c.csf(CSF_movecamera_y) && !c.scf(SCF_standby) && !math.IsInf(float64(c.pos[1]), 0) {
 			sys.cam.highest = MinF(c.interPos[1]*c.localscl, sys.cam.highest)
 			sys.cam.lowest = MaxF(c.interPos[1]*c.localscl, sys.cam.lowest)
 			sys.cam.Pos[1] = 0
+			// Mugen ignores characters that have infinite position
+			// https://github.com/ikemen-engine/Ikemen-GO/issues/1917
 		}
 	}
 }

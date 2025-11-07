25c1adce84b1ee2688ae25ff1d96930e5b3cd7a9
Lint Action <lint-action@samuelmeuli.com>
2025-11-07 22:11:31 +0000

    style: fix code style issues with gofmt

--- src/image.go
+++ src/image.go
@@ -716,8 +716,8 @@ func (s *Sprite) GetPalTex(pl *PaletteList) Texture {
 		return nil
 	}
 	if s.palidx < 0 || int(s.palidx) >= len(pl.paletteMap) {
-        s.palidx = 0
-    }
+		s.palidx = 0
+	}
 	return pl.PalTex[pl.paletteMap[int(s.palidx)]]
 }
 
